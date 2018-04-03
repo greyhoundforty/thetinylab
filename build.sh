307cb108bf658074d0fc7bfb2ab50c6d151e6f86
greyhoundforty <ryantiffany@fastmail.com>
2018-04-03 15:26:49 -0500

    Testing post install hook

--- build.sh
+++ build.sh
@@ -10,10 +10,13 @@ rm -rf _site
 
 jekyll build -d _site
 
+rm -f deployment.yml
+cp base-deployment.yml deployment.yml
+
 docker login -u "$DOCKER_HUB_USERNAME" -p "$DOCKER_HUB_PASSWORD"
 
 docker build -t docker.io/${DOCKER_HUB_USERNAME}/blogtinylab:${DOCKERTAG} .
 
 docker push docker.io/${DOCKER_HUB_USERNAME}/blogtinylab:${DOCKERTAG}
 
-sed -i "s|NEWTAG|${DOCKERTAG}|g" deployment.yml
+sed -i -e "s|NEWTAG|$DOCKERTAG|g" deployment.yml
